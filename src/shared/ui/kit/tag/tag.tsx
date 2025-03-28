85dbb440f66ccb78791d473e7638577b12bd3c3d
daniil-b2be <dan.developer@born2be.media>
2025-03-28 11:41:32 +0200

    tag anim

--- src/shared/ui/kit/tag/tag.tsx
+++ src/shared/ui/kit/tag/tag.tsx
@@ -5,11 +5,19 @@ import { ReactNode } from 'react';
 import st from './tag.module.scss';
 import { BlinkEllipse } from '@/shared/ui/icons';
 
-export function Tag({ children }: { children: ReactNode }) {
+export function Tag({
+  children,
+  width = 226,
+}: {
+  children: ReactNode;
+  width?: number;
+}) {
   return (
-    <span className={st.tag}>
-      <BlinkEllipse />
-      {children}
+    <span className={st.tag} style={{ width: `${width}px` }}>
+      <span className={st.children}>
+        <BlinkEllipse />
+        {children}
+      </span>
     </span>
   );
 }
